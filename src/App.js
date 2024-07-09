970d73fe8b7b026e510567de6b6c8d84af4aef80
malobs <malobamercy958@gmail.com>
2024-07-10 00:14:37 +0300

    ui modification

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
 
 
 
 function App() {
+  const[list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  
+
+  const addTodo = (todo) => {
+    const newTodo = {
+      id: Math.random(),
+      todo: todo,
+    };
+    setList([...list, newTodo]);
+
+    setInput('');
+  };
+
+  const deleteTodo = (id) => {
+    const newList = list.filter((todo)=> todo.id !==id);
+    setList(newList);
+
+  }
+
   return (
     <div>
-GOOD
+<h2>TODO LIST</h2>
+<input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+<button onClick={()=>addTodo(input)}>Add</button>
+<ul>
+  {list.map((todo)=>(
+    <li key ={todo.id}>{todo.todo}
+    <button onClick={()=>deleteTodo(todo.id)}>&times;</button></li>
+    
+  ))}
+</ul>
       
     </div>
   );
 }
 
 export default App;
+     
