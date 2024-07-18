fcc98f6767a2de72ed5a8b0897d08275dc4a83b2
malobs <malobamercy958@gmail.com>
2024-07-18 23:42:14 +0300

    change the structure of the code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+import TodoList from "./TodoList";
 
 
 
-function App() {
-  const[list, setList] = useState([]);
-  const [input, setInput] = useState("");
+function App() { 
+  return(
+    <div><TodoList /></div>
+  )
+//   const[list, setList] = useState([]);
+//   const [input, setInput] = useState("");
   
 
-  const addTodo = (todo) => {
-    const newTodo = {
-      id: Math.random(),
-      todo: todo,
-    };
-    setList([...list, newTodo]);
+//   const addTodo = (todo) => {
+//     const newTodo = {
+//       id: Math.random(),
+//       todo: todo,
+//     };
+//     setList([...list, newTodo]);
 
-    setInput('');
-  };
+//     setInput('');
+//   };
 
-  const deleteTodo = (id) => {
-    const newList = list.filter((todo)=> todo.id !==id);
-    setList(newList);
+//   const deleteTodo = (id) => {
+//     const newList = list.filter((todo)=> todo.id !==id);
+//     setList(newList);
 
-  }
+//   }
 
-  return (
-    <div>
-<h2>TODO LIST</h2>
-<input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-<button onClick={()=>addTodo(input)}>Add</button>
-<ul>
-  {list.map((todo)=>(
-    <li key ={todo.id}>{todo.todo}
-    <button onClick={()=>deleteTodo(todo.id)}>&times;</button></li>
+//   return (
+//     <div>
+// <h2>TODO LIST</h2>
+// <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+// <button onClick={()=>addTodo(input)}>Add</button>
+// <ul>
+//   {list.map((todo)=>(
+//     <li key ={todo.id}>{todo.todo}
+//     <button onClick={()=>deleteTodo(todo.id)}>&times;</button></li>
     
-  ))}
-</ul>
+//   ))}
+// </ul>
       
-    </div>
-  );
+//     </div>
+//   );
 }
 
 export default App;
---
+++ src/TodoList.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+const TodoList = () => {
+const [tasks, setTasks] = useState([]);
+const [newTask, setNewTask] = useState('');
+
+function handleInputChange (e) {
+    setNewTask(e.target.value);
+
+}
+function addTask (index) {
+    if(newTask.trim() !== ('')){
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+    }
+}
+function deleteTask (index) {
+    const updatedTasks = tasks.filter((_, i)=> i !== index);
+    setTasks(updatedTasks);
+
+}
+function moveTaskUp (index) {
+    if(index>0){
+        const updatedTasks = [...tasks];
+        [updatedTasks[index],updatedTasks[index-1]] = [updatedTasks[index-1], updatedTasks[index]];
+        setTasks(updatedTasks);
+
+    }
+
+
+}
+function moveTaskDown (index){
+    if(index< tasks.length - 1){
+        const updatedTasks = [...tasks];
+        [updatedTasks[index],updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]];
+        setTasks(updatedTasks);
+
+    }
+
+}
+
+  return (
+    <div className='to-do-list'>
+        <h1>Todo List</h1>
+        <input type='text' placeholder='Enter task' value={newTask} onChange={handleInputChange}/>
+        <button className='add-button' onClick={addTask}>ADD</button>
+
+        <ol>{tasks.map((task, index)=>
+        <li key={index}>
+            <span className='text'>{task}</span>
+            <button className='delete-button' onClick={()=>deleteTask(index)}>Delete</button>
+            <button className='move-button' onClick={()=>moveTaskUp(index)}>👆</button>
+            <button className='move-button' onClick={()=>moveTaskDown(index)}>👇</button>
+        </li>
+        
+        )}</ol>
+    </div>
+
+  )
+}
+
+export default TodoList
